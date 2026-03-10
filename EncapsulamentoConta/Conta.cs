8becfafb7c4b9e3642973b42a0e8cc2d15960f31
kauaanrs <kauaanrs16@gmail.com>
2026-03-09 21:55:35 -0300

    Encapsulamento

--- EncapsulamentoConta/Conta.cs
+++ EncapsulamentoConta/Conta.cs
@@ -8,9 +8,45 @@ namespace EncapsulamentoConta
     public class Conta
     {
         //declaração de atributos
-        public int numero;
-        public string titular;
-        public double saldo;
+        private int numero;
+        private string? titular;
+        private double saldo;
+
+        public int Numero //propriedade ; por isso maiusculo
+        {
+            set
+            {//value significa qualquer tipo 
+                this.numero = value;
+            }
+            get
+            {
+                return this.numero;
+            }
+        }
+        public string? Titular
+        {
+            get
+            {
+                return this.titular;
+            }
+            set
+            {
+                this.numero = value;
+            }
+        }
+        public double Saldo 
+        {
+            set
+            {
+                this.saldo = value;
+            }
+            get 
+            {
+                return this.saldo;
+            }
+        }
+
+
 
         //declaração de funções/métodos
         public void Sacar(double valorSacar){
--- EncapsulamentoConta/Program.cs
+++ EncapsulamentoConta/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EncapsulamentoConta;
+Conta c = new Conta();
+c.Numero = 1; //setNumero
+Console.WriteLine("Numero da conta: " + c.Numero); //getNumero
+c.Sacar(10);
+c.MostrarAtributos();
+//if (c.Numero > 5)
+//x = c.Numero + 21; 
