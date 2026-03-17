8425e1af8e0da9768bc6d83d4bab8477491fd4a0
kauaanrs <kauaanrs16@gmail.com>
2026-03-16 21:41:20 -0300

    .

---
+++ ConstrutorConta/Conta.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstrutorConta
+{
+    public class Conta
+    {
+        private int numero;
+        private string? titular;
+
+        private double saldo;
+
+        public Conta() //construtor padrão
+        { // não tem parametro e é concedido automaticamente pela classe
+        }
+
+        public Conta(int numero)
+        { //  atributo = parâmetro
+            this.numero = numero;
+        }
+        public Conta(string? titular, double saldo)
+        {
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+        public void MostrarAtributos()
+        {
+            Console.WriteLine("Número: " + numero +
+                        "\tTitular: " + titular +
+                        "\tSaldo: " + saldo);
+        }
+    }
+}
---
+++ ConstrutorConta/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConstrutorConta;
+
+Conta c1 = new Conta(); //chamada do construtor padrão
+c1.MostrarAtributos();
+
+Conta c2 = new Conta(1);
+c2.MostrarAtributos(); //chamada do construtor com 1 parametro
+
+Conta c3 = new Conta("Ana", 100);
+c3.MostrarAtributos();
